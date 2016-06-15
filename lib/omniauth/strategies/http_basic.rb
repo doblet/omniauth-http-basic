f4e3062f4dccda265b1b2ff50dcf82522301be15
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2016-06-15 07:59:57 -0400

    WIP
    Ale and I spent some time spiking out our own http basic omni strategy for the
    Doblet API.

--- lib/omniauth/strategies/http_basic.rb
+++ lib/omniauth/strategies/http_basic.rb
@@ -11,49 +11,37 @@ module OmniAuth
       option :title,   "Http Basic"
       option :headers, {}
 
-      def request_phase
-        OmniAuth::Form.build(:title => options.title, :url => callback_path) do
-          text_field 'Username', 'username'
-          password_field 'Password', 'password'
-        end.to_response
-      end
+      # NOT IMPLEMENTED FOR HTTP BASIC
+      # There's no need to make a request as the username and password are
+      # encoded in the HTTP Authentication header.
+      # def request_phase
+      # end
 
       def callback_phase
         return fail!(:invalid_credentials) if !authentication_response
-        return fail!(:invalid_credentials) if authentication_response.code.to_i >= 400
         super
       end
 
       protected
 
-        # by default we use static uri. If dynamic uri is required, override
-        # this method.
-        def api_uri
-          options.endpoint
+        def user_name_and_password
+          # TODO: hard dependency on Rails. is this okay?
+          @user_name_and_password ||= HttpAuthentication::Basic.user_name_and_password
         end
 
         def username
-          request['username']
+          user_name_and_password.first
         end
 
         def password
-          request['password']
+          user_name_and_password.last
         end
 
         def authentication_response
           unless @authentication_response
             return unless username && password
 
-            uri = URI(api_uri)
-            http = Net::HTTP.new(uri.host, uri.port)
-            if uri.scheme == 'https'
-              http.use_ssl = true
-              http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-            end
-
-            req = Net::HTTP::Get.new(uri.request_uri)
-            req.basic_auth username, password
-            @authentication_response = http.request(req)
+            @authentication_response = User.authenticate(username, password)
           end
 
           @authentication_response
